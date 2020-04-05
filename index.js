45d154ab8ab1416f75a3835a99040173ab00bd8d
luisandia <luisandiamamani@gmail.com>
2020-04-05 00:46:49 -0500

    add splash

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import App from './App';
 import {name as appName} from './app.json';
-import { YellowBox } from 'react-native';
+
+// Do stuff and hide the splash when you want
+setTimeout(() => {
+  SplashScreen.hide();
+}, 500);
 YellowBox.ignoreWarnings(['Setting a timer']);
 AppRegistry.registerComponent(appName, () => App);
